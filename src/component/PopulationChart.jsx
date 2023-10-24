d4a1b933437765b0ed4f2c6f28f55983eb97a23e
chawthinsuk <chawbanthin@gmail.com>
2023-10-24 13:26:09 +0700

    fix:api

--- src/component/PopulationChart.jsx
+++ src/component/PopulationChart.jsx
@@ -77,7 +77,7 @@ export function PopulationChart() {
       const worldResults = await axios.post(
         "https://zany-puce-dhole-slip.cyclic.app/country/get-total-by-year"
       );
-      let worldData = worldResults.data.results;
+      let worldData = worldResults.data.worldPopulation;
       let data = results.data.results;
       data = data.map((item, index) => {
         return {

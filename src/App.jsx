19d891ef69a96cf6130ffd279914fcf39ab46404
chawthinsuk <chawbanthin@gmail.com>
2023-10-24 17:53:43 +0700

    fix:initial data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import PopulationChart from "./component/PopulationChart";
+import { Test } from "./component/test";
 
 export function App() {
   return (
     <PopulationChart />
+    // <Test />
   )
 }
 export default App;
--- src/component/PopulationChart.jsx
+++ src/component/PopulationChart.jsx
@@ -24,6 +24,7 @@ export function PopulationChart() {
     "Oceania",
     "Europe",
   ]);
+  const [test, setTest] = useState();
 
   useInterval(() => {
     let currentYear = 1950 + iteration;
@@ -96,16 +97,6 @@ export function PopulationChart() {
     } catch (error) {
       console.log(error);
     }
-  };
-
-  const options = {
-    width: 650,
-    height: 550,
-    bar: { groupWidth: "80%" },
-    legend: { position: "none" },
-    titleTextStyle: {
-      fontSize: 19,
-    },
   };
 
   useEffect(() => {
@@ -242,7 +233,7 @@ export function PopulationChart() {
             chartType="BarChart"
             width="100%"
             height="100%"
-            data={data}
+            data={data || initialData}
             options={options}
           />
         </div>
@@ -299,3 +290,14 @@ function findContinent(country) {
     return "Unknown";
   }
 }
+
+const initialData = [["Element","Population",{"role":"style"},{"sourceColumn":0,"role":"annotation","type":"string","calc":"stringify"}],["China",543979200,"color: #5d45ea","🇨🇳"],["India",357021120,"color: #5d45ea","🇮🇳"],["United States",148281550,"color: #fccd35","🇺🇸"],["Russia",102580110,"color: #a26ce9","🇷🇺"],["Japan",84353060,"color: #5d45ea","🇯🇵"],["Germany",70964104,"color: #a26ce9","🇩🇪"],["Indonesia",69567624,"color: #5d45ea","🇮🇩"],["Brazil",53955360,"color: #fccd35","🇧🇷"],["United Kingdom",50055068,"color: #a26ce9","🇬🇧"],["Italy",46391944,"color: #a26ce9","🇮🇹"],["France",41842356,"color: #a26ce9","🇫🇷"],["Bangladesh",39728540,"color: #5d45ea","🇧🇩"]]
+const options = {
+  width: 650,
+  height: 550,
+  bar: { groupWidth: "80%" },
+  legend: { position: "none" },
+  titleTextStyle: {
+    fontSize: 19,
+  },
+};
